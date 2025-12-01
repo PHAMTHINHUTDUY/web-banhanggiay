0b53954c3d71228ae861100b3a2d683ed5b50a51
duypham <duypham@DESKTOP-NC2PPBP>
2025-12-01 19:19:39 +0700

    tạo trang Servlet

---
+++ src/main/java/dao/DAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.util.List;
+
+import entity.Category;
+import entity.Product;
+
+public class DAO {
+
+	public List<Product> getAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Category> getAllCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Product getLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
